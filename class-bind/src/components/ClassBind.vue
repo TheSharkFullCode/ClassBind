b3d4d02147e0930c13eb67364d2ed80d016eb5ba
TheSharkFullCode <agedjoscar@hotmail.com>
2024-04-28 17:55:13 +0200

    add new button

---
+++ class-bind/src/components/ClassBind.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>Welcome Oscar Danilo</h1>
+  <span v-bind:class="{'danger':hasError}">El mensaje se ha enviado satisfactoriamente</span> <br> <br>
+
+  <span :style="{backgroundColor:'pink',color:currentColor}">El mensaje se ha enviado satisfactoriamente</span> <br>
+  
+  <hr>
+        <button @click="handleClick()">
+            Cambiar color
+        </button>
+    </div>
+
+</template>
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue';
+    import { ref } from 'vue';
+
+        export default defineComponent({
+            name:'ClassBind',
+
+            setup(){
+
+                let hasError = true;
+                let alterColor = "#FFFF00";
+                let firtsColor = '#000000';
+                let currentColor = ref(firtsColor);
+
+                const handleClick = ()=>{
+
+                    currentColor.value = currentColor.value === firtsColor ? alterColor : firtsColor;
+                } 
+                
+                return {hasError,
+                    currentColor,
+                    handleClick,
+                    firtsColor};
+                
+
+            },
+
+            
+
+        })
+
+</script>
+
+<style scoped>
+.danger{
+color: red;
+}
+</style>
